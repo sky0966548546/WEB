e4a90c1d9c11791ba4314eec679305b72c0fbc9e
oF <60073419+sky0966548546@users.noreply.github.com>
2021-05-15 22:08:55 +0800

    Add files via upload

--- js/reset.js
+++ js/reset.js
@@ -1,7 +1,12 @@
 function reset() {
     try {
       document.createEvent("TouchEvent");
-      document.body.style.display = "none";
+      document.getElementById('true').style.display = "none";
+      document.getElementById('error').style.display="block";
+      document.body.style.backgroundColor="white";
+      document.body.style.backgroundImage="url('')";
+      document.body.style.color="black";
+      document.body.style.fontSize="";
       return true;
     } catch (e) {
       return false;
